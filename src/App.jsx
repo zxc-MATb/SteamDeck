bf0c9b548f4a62a54d041fa2d00de556eeb2aa67
Виталий <vitalikmarchukhv@gmail.com>
2024-10-11 20:30:43 +0300

    Создал и настроил проект на ReactJS

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+        </div>
+    );
+}
+
+export default App;
+
